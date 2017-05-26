4fef1d376f5c426c0387aad2c906e13ec712f7b5
Kenneth Korcal <kakorcal@gmail.com>
2017-05-26 15:22:24 -0700

    FP-753 adding fmb and responsive images

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ResponsiveImage, ResponsiveImageSize } from 'react-responsive-image';
 import './App.css';
 
 class App extends Component {
@@ -6,13 +7,26 @@ class App extends Component {
     return (
       <div className='App'>
         <div className='App__Hero'>
-          <div className='App__Hero__Button-Group'>
-            <button className='App__Button'>LEARN MORE</button>
-            <button className='App__Button fundpaas-widget-invOn__investButton'
-                    type="button" 
-                    data-offering-id="INVOFF-9P51M2VDU7U8I" 
-                    disabled
-            >INVEST</button>
+          <div className="App__Hero-Banner-Group">
+            <ResponsiveImage>
+              <ResponsiveImageSize
+                default
+                minWidth={0}
+                path={'./assets/images/main.jpg'}
+              />
+              <ResponsiveImageSize
+                minWidth={1200}
+                path={'./assets/images/banner.png'}
+              />
+            </ResponsiveImage>
+            <div className='App__Hero__Button-Group'>
+              <button className='App__Button'>LEARN MORE</button>
+              <button className='App__Button fundpaas-widget-invOn__investButton'
+                      type="button" 
+                      data-offering-id="INVOFF-495HM68QL2CNC" 
+                      disabled
+              >INVEST</button>
+            </div>
           </div>
         </div>
       </div>
