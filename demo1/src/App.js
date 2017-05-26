3ef2e9a4127da4cd50c4ac4bc024de67ab37fbb6
Kenneth Korcal <kakorcal@gmail.com>
2017-05-26 11:03:49 -0700

    FP-753 create main page

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>ONE ROQ</h2>
+      <div className='App'>
+        <div className='App__Hero'>
+          <div className='App__Hero__Button-Group'>
+            <button className='App__Button'>Learn More</button>
+            <button className='App__Button'>Invest</button>
+          </div>
         </div>
       </div>
     );
