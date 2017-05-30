883196f60679c16a8b8e82e25dc7be4c46ee453a
Kenneth Korcal <kakorcal@gmail.com>
2017-05-30 04:23:31 -0700

    FP-753 signing up investor

--- demo2/src/components/Signup.js
+++ demo2/src/components/Signup.js
@@ -1,61 +1,115 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { isEmpty } from 'lodash';
 
 class Signup extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      fileName: ""
+      fileName: "",
+      errorMessage: ""
     };
   }
 
   handleFileChange(e) {
-    this.setState({ fileName: e.target.files[0].name });
+    if(e.target.files.length) {
+      this.setState({ fileName: e.target.files[0].name });
+    }else {
+      this.setState({ fileName: '' });
+    }
   }
 
   handleFormSubmit(e) {
-    var payload = {};
-    var inputValues = document.getElementsBy('input');
+    e.preventDefault();
+    var validationMessage = '';
+    var formEl = document.forms.signupForm;
+    var formData = new FormData(formEl);
 
-    for(var i = 0; i < inputValues.length; i++) {
-      var name = inputValues[i].name;
-      var value = inputValues[i].value;
-      payload.name = value;
-    }
-
-  /* example payload
-    {
-      "username": "foobar@gmail.com",
-      "investorType": "PA",
-      "firstName": "John",
-      "lastName": "Doe",
-      "phoneNumber": "+1 914-555-1212",
-      "dob": [
-        1945,
-        9,
-        2
-      ],
-      "address": {
-        "streetAddress1": "2800 Sand Hill Rd",
-        "streetAddress2": "#101",
-        "city": "Menlo Park",
-        "usState": "CA",
-        "postalCode": "94025",
-        "country": "US"
+    var payloadData = new FormData();
+    var investor = {
+      investorType: "PA",
+      address: {
+        country: "US"
       },
-      "ssn": "1111",
-      "deviceFingerprint": "API-I-KScsbfEApVYWfvQXvYgK"
+      deviceFingerprint: "API-I-KScsbfEApVYWfvQXvYgK"
+    };
+
+    formData.forEach((value, key) => {
+      if(isEmpty(value) && isEmpty(validationMessage)) {
+        var element = document.getElementById(key);
+        if(key === 'driversLicense') {
+          if(!value.name) {
+            validationMessage = element.dataset.label + ' is required';
+          }
+        }else {
+          if(element.dataset.label) {
+            validationMessage = element.dataset.label + ' is required';
+          }
+        }
+
+      }
+
+      switch(key) {
+        case 'username':
+        case 'password':
+        case 'firstName':
+        case 'lastName':
+        case 'phoneNumber':
+        case 'ssn':
+          investor[key] = value;
+          break;
+        case 'dob':
+          var dob = value.split('-').map(num => +num);
+          if(dob.length === 3) {
+            investor[key] = dob.map(num => +num);
+          }else {
+            // need to fix validation on api side to handle bad input like [0,0,0]
+            investor[key] = [1500,1,1];
+          }
+          break;
+        case 'driversLicense':
+          payloadData.append("DrLcn", value);
+          break;
+        default:
+          investor.address[key] = value;
+          break;
+      }
+    });
+
+    payloadData.append("investor", JSON.stringify(investor));
+    
+    if(!validationMessage) {
+      fetch('http://local.fundpaas.com:8080/api/logins/signups/investors', {
+        method: 'POST',
+        body: payloadData
+      })
+      .then(promise => promise.json())
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          errorMessage: response.customerMessage
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          errorMessage: 'Failed to sent data. Please contact support.'
+        });
+      });
+    }else {
+      this.setState({
+        errorMessage: validationMessage
+      });
     }
-    */
   }
 
   render(){
     return (
       <div className='App__Container'>
-        <div className="App__Signup">
+        <form className="App__Signup" id='signupForm' onSubmit={this.handleFormSubmit.bind(this)}>
           <h1>Signup</h1>
           <hr />
-          <div className='App__Notification' />
+          <div className={this.state.errorMessage ? 'App__Notification' : ''}>{this.state.errorMessage}</div>
           <table className="App__Signup__Form">
             <thead className='App__Signup__Form__Instructions'>
               <tr>
@@ -66,11 +120,11 @@ class Signup extends Component{
               <tr>
                 <td>
                   <label htmlFor="username">Personal email</label>
-                  <input type="email" name="username" id="username" />
+                  <input type="email" name="username" id="username" data-label='Personal email' />
                 </td>
                 <td>
                   <label htmlFor="password">Password</label>
-                  <input type="password" name="password" id="password" />
+                  <input type="password" name="password" id="password" data-label='Password' />
                 </td>
               </tr>
             </tbody>
@@ -78,27 +132,27 @@ class Signup extends Component{
               <tr>
                 <td>
                   <label htmlFor="firstName">First name</label>
-                  <input type="text" name="firstName" id="firstName" />
+                  <input type="text" name="firstName" id="firstName" data-label='First name' />
                 </td>
                 <td>
                   <label htmlFor="lastName">Last name</label>
-                  <input type="text" name="lastName" id="lastName" />
+                  <input type="text" name="lastName" id="lastName" data-label='Last name' />
                 </td>
               </tr>
 
               <tr>
                 <td>
                   <label htmlFor="phoneNumber">Personal phone</label>
-                  <input type="tel" name="phoneNumber" id="phoneNumber" placeholder="+1 000-000-0000" />
+                  <input type="tel" name="phoneNumber" id="phoneNumber" data-label='Personal phone' placeholder="+1 000-000-0000" />
                 </td>
                 <td className='App__Signup__Form__Col--2'>
                   <div>
                     <label htmlFor="dob">Date of birth</label>
-                    <input type="date" name="dob" id="dob" className='App__Signup__Form__Col--2--Shift' />                    
+                    <input type="date" name="dob" id="dob" data-label='Date of birth' className='App__Signup__Form__Col--2--Shift' />                    
                   </div>
                   <div>
                     <label htmlFor="ssn">SSN (Last 4 Digits)</label>
-                    <input type="text" name="ssn" id="ssn" />
+                    <input type="text" name="ssn" id="ssn" data-label='SSN' />
                   </div>
                 </td>
               </tr>
@@ -106,7 +160,7 @@ class Signup extends Component{
               <tr>
                 <td colSpan={2}>
                   <label htmlFor="streetAddress1">Personal address</label>
-                  <input type="text" name="streetAddress1" id="streetAddress1" placeholder="e.g. 123 Main St" />
+                  <input type="text" name="streetAddress1" id="streetAddress1" data-label='Personal address' placeholder="e.g. 123 Main St" />
                 </td>
               </tr>
 
@@ -119,18 +173,18 @@ class Signup extends Component{
               <tr>
                 <td>
                   <label htmlFor="city">City</label>
-                  <input type="text" name="city" id="city" />
+                  <input type="text" name="city" id="city" data-label='City' />
                 </td>
                 <td className='App__Signup__Form__Col--2'>
                   <div>
                     <label htmlFor="usState">State</label>
-                    <select name="usState" id="usState" className='App__Signup__Form__Col--2--Shift'>
-                      <option disabled="disabled" selected="selected" value=""></option><option value="AL">AL</option><option value="AK">AK</option><option value="AZ">AZ</option><option value="AR">AR</option><option value="CA">CA</option><option value="CO">CO</option><option value="CT">CT</option><option value="DE">DE</option><option value="DC">DC</option><option value="FL">FL</option><option value="GA">GA</option><option value="HI">HI</option><option value="ID">ID</option><option value="IL">IL</option><option value="IN">IN</option><option value="IA">IA</option><option value="KS">KS</option><option value="KY">KY</option><option value="LA">LA</option><option value="ME">ME</option><option value="MD">MD</option><option value="MA">MA</option><option value="MI">MI</option><option value="MN">MN</option><option value="MS">MS</option><option value="MO">MO</option><option value="MT">MT</option><option value="NE">NE</option><option value="NV">NV</option><option value="NH">NH</option><option value="NJ">NJ</option><option value="NM">NM</option><option value="NY">NY</option><option value="NC">NC</option><option value="ND">ND</option><option value="OH">OH</option><option value="OK">OK</option><option value="OR">OR</option><option value="PA">PA</option><option value="RI">RI</option><option value="SC">SC</option><option value="SD">SD</option><option value="TN">TN</option><option value="TX">TX</option><option value="UT">UT</option><option value="VT">VT</option><option value="VA">VA</option><option value="WA">WA</option><option value="WV">WV</option><option value="WI">WI</option><option value="WY">WY</option><option value="AS">AS</option><option value="GU">GU</option><option value="MP">MP</option><option value="PR">PR</option><option value="VI">VI</option><option value="FM">FM</option><option value="MH">MH</option><option value="PW">PW</option><option value="AA">AA</option><option value="AE">AE</option><option value="AP">AP</option>
+                    <select name="usState" id="usState" data-label='State' className='App__Signup__Form__Col--2--Shift'>
+                      <option selected disabled value=""></option><option value="AL">AL</option><option value="AK">AK</option><option value="AZ">AZ</option><option value="AR">AR</option><option value="CA">CA</option><option value="CO">CO</option><option value="CT">CT</option><option value="DE">DE</option><option value="DC">DC</option><option value="FL">FL</option><option value="GA">GA</option><option value="HI">HI</option><option value="ID">ID</option><option value="IL">IL</option><option value="IN">IN</option><option value="IA">IA</option><option value="KS">KS</option><option value="KY">KY</option><option value="LA">LA</option><option value="ME">ME</option><option value="MD">MD</option><option value="MA">MA</option><option value="MI">MI</option><option value="MN">MN</option><option value="MS">MS</option><option value="MO">MO</option><option value="MT">MT</option><option value="NE">NE</option><option value="NV">NV</option><option value="NH">NH</option><option value="NJ">NJ</option><option value="NM">NM</option><option value="NY">NY</option><option value="NC">NC</option><option value="ND">ND</option><option value="OH">OH</option><option value="OK">OK</option><option value="OR">OR</option><option value="PA">PA</option><option value="RI">RI</option><option value="SC">SC</option><option value="SD">SD</option><option value="TN">TN</option><option value="TX">TX</option><option value="UT">UT</option><option value="VT">VT</option><option value="VA">VA</option><option value="WA">WA</option><option value="WV">WV</option><option value="WI">WI</option><option value="WY">WY</option><option value="AS">AS</option><option value="GU">GU</option><option value="MP">MP</option><option value="PR">PR</option><option value="VI">VI</option><option value="FM">FM</option><option value="MH">MH</option><option value="PW">PW</option><option value="AA">AA</option><option value="AE">AE</option><option value="AP">AP</option>
                     </select>
                   </div>
                   <div>
                     <label htmlFor="postalCode">Zip code</label>
-                    <input type="text" name="postalCode" id="postalCode" />
+                    <input type="text" name="postalCode" id="postalCode" data-label='Zip code' />
                   </div>
                 </td>
               </tr>
@@ -142,6 +196,7 @@ class Signup extends Component{
                     <input type="file" 
                            name="driversLicense" 
                            id="driversLicense" 
+                           data-label="Driver's license"
                            onChange={this.handleFileChange.bind(this)}/>
                     <div>Choose file</div>
                     <span>{this.state.fileName}</span>
@@ -154,9 +209,9 @@ class Signup extends Component{
             <Link to='/'>
               <button>Back</button>
             </Link>
-            <button onClick={this.handleFormSubmit.bind(this)}>Continue</button>
+            <button type='submit'>Continue</button>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
--- demo2/src/redux/store.js
+++ demo2/src/redux/store.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
-console.log(applyMiddleware);
 const store = createStore(
   rootReducer,
   compose(
